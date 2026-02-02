bf8e73423106e1a7b72384ebc740e0e8bbe249da
Tomas Ruiz <ruiztms20@gmail.com>
2026-02-02 16:48:22 -0300

    Arreglé el problema de tailwind al momento de compilar

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-zenitBlack">
+      <Text className="text-zenitNeon text-3xl font-bold">Zenit Renacido 🚀</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        zenitBlack: '#121212',
+        zenitNeon: '#D4FF00', 
+        zenitDarkGray: '#1E1E1E',
+      },
+    },
+  },
+  plugins: [],
+}
